497dcbfc043d93d244f10420cb9342243b0e80d6
jvojta <jvojta2005@gmail.com>
2024-04-18 07:05:14 +0000

    Init

--- pr9/main.cpp
+++ pr9/main.cpp
@@ -1,20 +1,7 @@
-#include "retezce.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  char s[10] = "Ahoj!";
-  char s2[10] = "ho";
-  // s="Ahoj"; ###NELZE!###
-  // strcpy(s, "Ahoj!"); ###NEJLIP, #include <cstring>###
-  prnt(s);
-  strdel(s, 1, 2);
-  prnt(s);
-  strins(s, s2, 1);
-  prnt(s);
-  strdel(s, 1, 2);
-  prnt(s);
-  strinsII(s, s2, 1);
-  prnt(s);
+  cout << "Hello World!\n";
 }
